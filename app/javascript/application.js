4d6c6de719a9a07d8e15bd26a2c70a513d61ca58
Kevin Salamanca <kevinsalamanca26@gmail.com>
2023-10-04 12:18:19 -0400

    Add navigation partial and favicon

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,9 @@
+//= require semantic-ui
+//= require jquery
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import "@hotwired/turbo-rails";
+import "controllers";
+import jquery from "jquery";
+
+window.jQuery = jquery;
+window.$ = jquery;
